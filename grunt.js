74896e45fdc430fc0eb1721732cf1ab618092901
rma4ok <rma4ok@gmail.com>
2013-01-20 23:57:03 -0500

    [grunt] migrate to bg-shell

--- grunt.js
+++ grunt.js
@@ -1,20 +1,20 @@
 module.exports = function (grunt) {
   "use strict";
 
-  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-bg-shell');
 
 
   // Project configuration.
   grunt.initConfig({
 
-    shell: {
+    bgShell: {
       coffeeCompile: {
-        command: './node_modules/coffee-script/bin/coffee -b -o ./build -c ./src',
+        cmd: './node_modules/coffee-script/bin/coffee -b -o ./build -c ./src',
         stdout: true,
         stderr: true
       },
       coffeeWatch: {
-        command: './node_modules/coffee-script/bin/coffee -bw -o ./build -c ./src',
+        cmd: './node_modules/coffee-script/bin/coffee -bw -o ./build -c ./src',
         stdout: true,
         stderr: true
       }
@@ -22,15 +22,7 @@ module.exports = function (grunt) {
   });
 
 
-  grunt.registerTask('default', 'Watching coffee', function () {
-    // Force task into async mode and grab a handle to the "done" function.
-    var done = this.async();
-    // Run some sync stuff.
-
-    grunt.task.run('shell:coffeeWatch');
-    done();
-
-  });
+  grunt.registerTask('default', 'bgShell:coffeeWatch');
 
 
 };
